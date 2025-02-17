1462eb2174d57574b6ed138db6b0efddc645b26f
Isabel <83420259+isabel-mm@users.noreply.github.com>
2025-02-17 20:40:28 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 from gestion_corpus import gestion_corpus
 from extraccion_terminos import extraccion_terminologica
 from validacion_terminos import validacion_terminos
+from acerca_de import acerca_de  # Importamos la sección "Acerca de"
 
 # Inicializar el estado de sesión para la navegación
 if "pagina" not in st.session_state:
@@ -11,20 +12,22 @@ if "pagina" not in st.session_state:
 # Cambiar el diseño del menú lateral con botones
 st.sidebar.markdown("### Navegador")
 
-if st.sidebar.button("🏠 Inicio"):
+if st.sidebar.button("Inicio"):
     st.session_state.pagina = "Inicio"
-if st.sidebar.button("📂 Gestión de corpus"):
+if st.sidebar.button("Gestión de corpus"):
     st.session_state.pagina = "Gestión de corpus"
-if st.sidebar.button("📊 Extracción terminológica"):
+if st.sidebar.button("Extracción terminológica"):
     st.session_state.pagina = "Extracción terminológica"
-if st.sidebar.button("✅ Validación de términos"):
+if st.sidebar.button("Validación de términos"):
     st.session_state.pagina = "Validación de términos"
+if st.sidebar.button("Acerca de"):
+    st.session_state.pagina = "Acerca de"  # Agregamos la opción "Acerca de" al final
 
 # ------------------------------
 # Funcionalidad 0: Pantalla de Inicio
 # ------------------------------
 if st.session_state.pagina == "Inicio":
-    st.title(" 🤖 TermoLing, una estación de trabajo terminográfico")
+    st.title("🤖 TermoLing, una estación de trabajo terminográfico")
     st.markdown(
         """
         👋 ¡Hola! Esta es una aplicación creada para ayudarte en la gestión y minería de textos, especialmente diseñada para asistirte en el trabajo terminográfico.
@@ -33,7 +36,7 @@ if st.session_state.pagina == "Inicio":
         
         - 📂 **Gestión de corpus** → Subir archivos `.txt` (¡siempre es mejor si está codificado en UTF-8!) y estructurar un corpus con metadatos.
         - 📊 **Extracción terminológica** → Extraer términos mediante métodos como **TF-IDF, POS Tagging y C-Value**.
-        - ✅ **Validación de términos** → Subir un archivo `.csv` con términos extraídos (¡el mismo que te genera esta app!) y cuáles de los candidatos a término son reales.
+        - ✅ **Validación de términos** → Subir un archivo `.csv` con términos extraídos (¡el mismo que te genera esta app!) y validar los candidatos a término.
         
         📌 **Usa el menú lateral para navegar entre las distintas funciones. ¡Espero que te sirva!**
         """
@@ -56,3 +59,9 @@ elif st.session_state.pagina == "Extracción terminológica":
 # ------------------------------
 elif st.session_state.pagina == "Validación de términos":
     validacion_terminos()
+
+# ------------------------------
+# Funcionalidad 4: Acerca de
+# ------------------------------
+elif st.session_state.pagina == "Acerca de":
+    acerca_de()
