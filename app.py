e570b24214c55b20d6f366e05fb41b5ec535d660
Isabel <83420259+isabel-mm@users.noreply.github.com>
2025-02-17 19:49:13 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,25 @@
-import streamlit as st
-from gestion_corpus import gestion_corpus
-from extraccion_terminos import extraccion_terminologica
-from validacion_terminos import validacion_terminos
-
 # Menú lateral para seleccionar la funcionalidad
-st.sidebar.title("Menú de funciones")
-opcion = st.sidebar.radio("", ["Gestión de corpus", "Extracción terminológica", "Validación de términos"])
+opcion = st.sidebar.radio("", ["Inicio", "Gestión de corpus", "Extracción terminológica", "Validación de términos"])
 
-# Ejecutar la funcionalidad seleccionada
-if opcion == "Gestión de corpus":
-    gestion_corpus()
-elif opcion == "Extracción terminológica":
-    extraccion_terminologica()
-elif opcion == "Validación de términos":
-    validacion_terminos()
+# ------------------------------
+# Funcionalidad 0: Pantalla de Inicio
+# ------------------------------
+if opcion == "Inicio":
+    st.title("📌 App para el trabajo terminográfico")
+
+    st.markdown(
+        """
+        👋 ¡Hola! Esta es una aplicación diseñada para ayudarte en la gestión y minería de textos, especialmente diseñada para asistirte en el trabajo terminográfico.
+        
+        🔍 **¿Qué puedes hacer aquí?**
+        
+        - 📂 **Gestión de corpus** → Subir tus archivos .txt (¡siempre es mejor en UFT-8!) y estructurar tu corpus en un dataset con sus correspondientes metadatos.
+        - 🏷️ **Extracción terminológica** → Extraer términos mediante distintos métodos como **TF-IDF, POS Tagging y C-Value** para identificar términos en tu corpus.
+        - ✅ **Validación de términos** → Subir un CSV con términos extraídos (¡el que te proporciona esta misma app! y marcar cuáles de ellos son términos reales.  
+        
+        📌 **Usa el menú lateral para navegar entre las funcionalidades.**
+        
+        """
+    )
+
+    st.image("https://media.giphy.com/media/hvRJCLFzcasrR4ia7z/giphy.gif", width=100)
